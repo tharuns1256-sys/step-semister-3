601fa05ac3bc4816cbaae05247b8b62a4df86ac6
tharuns1256-sys <tharuns1256@gmail.com>
2026-09-06 00:23:10 +0530

    Add Week 1 practice problems

---
+++ Practice Problems/ReverseCustomerName.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ReverseCustomerName {
+
+    static String reverseCustomerName(String customerName) {
+
+        char[] characters = customerName.toCharArray();
+
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = characters.length - 1; i >= 0; i--) {
+            reversed.append(characters[i]);
+        }
+
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+
+        String reversedName = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+
+        sc.close();
+    }
+}
---
+++ Practice Problems/RockPaperScissors.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class RockPaperScissors {
+
+    static String playRound(String playerMove, String computerMove) {
+        if (playerMove.equals(computerMove)) {
+            return "Draw";
+        }
+
+        if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+            (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+            (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
+            return "Player Wins";
+        }
+
+        return "Computer Wins";
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] moves = {"Rock", "Paper", "Scissors"};
+
+        String[] playerMoves = new String[5];
+        String[] computerMoves = new String[5];
+        String[] results = new String[5];
+
+        int wins = 0;
+        int losses = 0;
+        int draws = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.print("Round " + (i + 1) +
+                    " - Enter Rock, Paper, or Scissors: ");
+
+            String playerMove = sc.nextLine();
+
+            // Capitalize first letter
+            playerMove = playerMove.substring(0, 1).toUpperCase()
+                    + playerMove.substring(1).toLowerCase();
+
+            String computerMove = moves[random.nextInt(3)];
+
+            String result = playRound(playerMove, computerMove);
+
+            playerMoves[i] = playerMove;
+            computerMoves[i] = computerMove;
+            results[i] = result;
+
+            if (result.equals("Player Wins")) {
+                wins++;
+            } else if (result.equals("Computer Wins")) {
+                losses++;
+            } else {
+                draws++;
+            }
+
+            System.out.println("Computer: " + computerMove);
+            System.out.println("Result: " + result);
+            System.out.println();
+        }
+
+        double winPercentage = (wins / 5.0) * 100;
+
+        System.out.println("----- Final Summary -----");
+        System.out.println("Round\tPlayer Move\tComputer Move\tResult");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println((i + 1) + "\t" +
+                    playerMoves[i] + "\t\t" +
+                    computerMoves[i] + "\t\t" +
+                    results[i]);
+        }
+
+        System.out.println();
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Draws: " + draws);
+        System.out.println("Win Percentage: " + winPercentage + "%");
+
+        sc.close();
+    }
+}
